3b37d11a2bad007b48ab20465604b99c8dbc454a
imandato <imandato211@gmail.com>
2022-08-10 20:14:32 -0400

    adding index and show pages

---
+++ views/index.jsx
@@ -0,0 +1,24 @@
+const React = require('react');
+const Default = require('./layouts/default');
+
+const Index = ({breads, title}) => {
+    return(
+        <Default title={title}>
+            <h2>Index Page</h2>
+            <ul>
+                {breads.map((bread, index) =>{
+                    return(
+                        <li key={index}>
+                            <a href={`/breads/${index}`}>
+                                {bread.name}
+                            </a>
+                        </li>
+                    );
+                })}
+            </ul>
+            {/* <p>I have {breads[0].name} bread!</p> */}
+        </Default>
+    );
+};
+
+module.exports = Index
